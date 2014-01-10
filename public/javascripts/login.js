566405aa76e80480ae1339d75672013f3f0203d3
LeeYoungJu <dmlqhqhqh@naver.com>
2014-01-10 14:19:31 +0900

    IE

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -45,11 +45,7 @@
 					form.submit();					
 				}
 				
-				function go_register_form() {
-					if(navigator.appName == 'Microsoft Internet Explorer') {
-   					    alert('인터넷 익스플로어로는 사용 불가합니다. 죄송합니다.');   		   
-   		    			return;   			
-   	    			}  
+				function go_register_form() {					
 					window.location.href = '/register_form';
 				}
 			});
--- public/javascripts/wait_room.js
+++ public/javascripts/wait_room.js
@@ -210,11 +210,11 @@ $(document).ready(function() {
 		var e = e || window.event;
 		var title = $('#roomname').val();		
 		
-		if(navigator.appName == 'Microsoft Internet Explorer') {
+		/*if(navigator.appName == 'Microsoft Internet Explorer') {
    		    alert('인터넷 익스플로어로는 사용 불가합니다. 죄송합니다.');
    		    e.preventDefault();
    		    return;   			
-   	    }  
+   	    } */ 
    	    
    	    if(title.trim() == '' || !title) {
 			alert('제목을 입력하세요.');
@@ -343,10 +343,11 @@ $(document).ready(function() {
 	});
 	
 	function click_enter_button(e) {
+		/*
 		if(navigator.appName == 'Microsoft Internet Explorer') {
    		    alert('인터넷 익스플로어로는 사용 불가합니다. 죄송합니다.');   		   
    		    return;   			
-   	    }  
+   	    } */ 
 		var e = e || window.event;
 		var self = e.target;
 		var room = self.parentNode.parentNode.info;
